63e2bb179ee80d610547387063599fe7281ca07f
Robert de Forest <guitar.robot@gmail.com>
2018-06-27 20:37:09 -0700

    -

--- unmerged/initHost.sh
+++ unmerged/initHost.sh
@@ -6,12 +6,15 @@ sudo mkdir -p /package/source
 
 sudo chown `whoami` /package/source
 
-urls="$urls http://cr.yp.to/daemontools/daemontools-0.76.tar.gz"
-urls="$urls http://cr.yp.to/ucspi-tcp/ucspi-tcp-0.88.tar.gz"
-urls="$urls http://cr.yp.to/publicfile/publicfile-0.52.tar.gz"
+packages="daemontools-0.76 ucspi-tcp-0.88 publicfile-0.52"
+#urls="$urls http://cr.yp.to/daemontools/daemontools-0.76.tar.gz"
+#urls="$urls http://cr.yp.to/ucspi-tcp/ucspi-tcp-0.88.tar.gz"
+#urls="$urls http://cr.yp.to/publicfile/publicfile-0.52.tar.gz"
 
-for url in $urls; do
-  wget "$url"
+#for url in $urls; do
+for pkg in $packages; do
+  pkgdir=$(echo $pkg | sed 's/-.*//')
+  wget "http://cr.yp.to/$pkgdir/$pkg.tar.gz"
 done
 
 #...
